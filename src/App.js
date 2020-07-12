eb5757fc34623dfd54eaccff4599d78f5f9ff7a2
yakonba <yakonbagatulj@gmail.com>
2020-07-12 02:48:00 -0500

    Avance hasta video 99

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
-import React, { Component } from "react";
-import { Grid, Row, Col } from "react-flexbox-grid";
-import Paper from "@material-ui/core/Paper";
-import AppBar from "@material-ui/core/AppBar";
-import Typography from "@material-ui/core/Typography";
-import Toolbar from "@material-ui/core/Toolbar";
-import LocationList from "./components/LocationList";
-import ForecastExtended from "./components/ForecastExtended";
-import "./App.css";
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import LocationList from './components/LocationList';
+import ForecastExtended from './components/ForecastExtended';
+import './App.css';
 
 const cities = [
-  "Buenos Aires,ar",
-  "Washington,us",
-  "Bogota,col",
-  "Ciudad de México,mx",
-  "Madrid,es",
-  "Lima,pe"
+    'Buenos Aires,ar',
+    'Washington,us',
+    'Bogota,col',
+    'Ciudad de México,mx',
+    'Madrid,es',
+    'Lima,pe',
 ];
 
 class App extends Component {
-  handleSelectedLocation = city => {
-    console.log(`handleSelectedLocation ${city}`);
-  };
-  render() {
-    return (
-      <Grid>
-        <Row>
-          <AppBar position="sticky">
-            <Toolbar>
-              <Typography variant="subtitle1" color="inherit">
-                Weather App
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        </Row>
-        <Row>
-          <Col xs={12} md={6}>
-            <LocationList
-              cities={cities}
-              onSelectedLocation={this.handleSelectedLocation}
-            ></LocationList>
-          </Col>
-          <Col xs={12} md={6}>
-            <Paper elevation={4}>
-              <div className="detail">
-                <ForecastExtended city={"Buenos Aires,ar"}></ForecastExtended>
-              </div>
-            </Paper>
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
+    constructor() {
+        super();
+        this.state = { city: null };
+    }
+    handleSelectedLocation = (city) => {
+        this.setState({ city });
+        console.log(`handleSelectedLocation ${city}`);
+    };
+    render() {
+        const { city } = this.state;
+        return (
+            <Grid>
+                <Row>
+                    <AppBar position="sticky">
+                        <Toolbar>
+                            <Typography variant="subtitle1" color="inherit">
+                                Weather App
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                </Row>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <LocationList
+                            cities={cities}
+                            onSelectedLocation={this.handleSelectedLocation}
+                        ></LocationList>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <Paper elevation={4}>
+                            <div className="detail">
+                                {city && <ForecastExtended city={city}></ForecastExtended>}
+                            </div>
+                        </Paper>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
 
 export default App;
--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,15 +1,67 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
+import transformForecast from './../services/transformForecast';
+import './styles.css';
+
+// const days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+
+// const data = {
+//     temperature: 10,
+//     humidity: 10,
+//     weatherState: 'normal',
+//     wind: 'normal',
+// };
+
+const api_key = 'f5ccc128c66292e3b8ddaa1045c6387b';
+const url = 'http://api.openweathermap.org/data/2.5/forecast';
 
 class ForecastExtended extends Component {
-  render() {
-    const { city } = this.props;
-    return <div> Pronostico Extendido para {city}</div>;
-  }
+    constructor() {
+        super();
+        this.state = { forecastData: null };
+    }
+
+    componentDidMount() {
+        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+        fetch(url_forecast)
+            .then((data) => data.json())
+            .then((weather_data) => {
+                console.log(weather_data);
+                const forecastData = transformForecast(weather_data);
+                console.log(forecastData);
+                this.setState({ forecastData });
+            });
+    }
+
+    renderForecastItemDays(forecastData) {
+        return forecastData.map((forecast) => (
+            <ForecastItem
+                key={`${forecast.weekDay}${forecast.hour}`}
+                weekDay={forecast.weekDay}
+                hour={forecast.hour}
+                data={forecast.data}
+            ></ForecastItem>
+        ));
+    }
+    renderProgress() {
+        return <h3>Cargando Pronostico extendido...</h3>;
+    }
+
+    render() {
+        const { city } = this.props;
+        const { forecastData } = this.state;
+        return (
+            <div>
+                <h2 className="forecast-title"> Pronostico Extendido para {city}</h2>
+                {forecastData ? this.renderForecastItemDays(forecastData) : this.renderProgress()}
+            </div>
+        );
+    }
 }
 
 ForecastExtended.propTypes = {
-  city: PropTypes.string.isRequired
+    city: PropTypes.string.isRequired,
 };
 
 export default ForecastExtended;
